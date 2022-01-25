15c79df6a049474586f1f2d5fb2ca2192a5ccaf2
Muriel Damazio <murieldps@gmail.com>
2022-01-25 20:12:52 -0300

    Compliment Service, Controller and route done

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express'
 import { CreateTagController } from './controllers/CreateTagController'
 import { CreateUserController } from './controllers/CreateUserController'
 import { AuthenticateUserController } from './controllers/AuthenticateUserController'
+import { CreateComplimentController } from './controllers/CreateComplimentController'
 
 import { ensureAdmin } from './middlewares/ensureAdmin'
 
@@ -11,9 +12,11 @@ const router = Router()
 const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
 const authenticateUserController = new AuthenticateUserController()
+const createComplimentController = new CreateComplimentController()
 
 router.post("/users", createUserController.handle)
 router.post("/tags", ensureAdmin, createTagController.handle)
 router.post("/login", authenticateUserController.handle)
+router.post("/compliments", createComplimentController.handle)
 
 export { router }
---
+++ src/services/CreateComplimentService.ts
@@ -0,0 +1,41 @@
+import { getCustomRepository } from "typeorm"
+import { ComplimentRepositories } from "../repositories/ComplimentRepositories"
+import { UserRepositories } from "../repositories/UserRepositories"
+
+interface IComplimentRequest {
+  user_sender: string;
+  user_receiver: string;
+  tag_id: string;
+  message: string;
+}
+
+class CreateComplimentService {
+  async execute({ user_sender, user_receiver, tag_id, message }: IComplimentRequest) {
+    const complimentRepositories = getCustomRepository(ComplimentRepositories)
+    const userRepositories = getCustomRepository(UserRepositories)
+
+    if (user_sender === user_receiver) {
+      throw new Error("You can't send a compliment to yourself.")
+    }
+
+    const userReceiverExists = await userRepositories.findOne(user_receiver)
+
+    if (!userReceiverExists) {
+      throw new Error("User Receiver doesn't exists.")
+    }
+
+    const compliment = complimentRepositories.create({
+      user_sender,
+      user_receiver,
+      tag_id,
+      message
+    })
+
+    await complimentRepositories.save(compliment)
+
+    return compliment
+
+  }
+}
+
+export { CreateComplimentService }
